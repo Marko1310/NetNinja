6e03dc2c1a33dce507a3965806422f1ddf8b66bd
Marko1310 <marko.cabo13@gmail.com>
2022-11-30 18:08:56 +0100

    adding a basic list for the blog

--- netninja/src/Home.js
+++ netninja/src/Home.js
@@ -1,23 +1,25 @@
-const Home = () => {
-  const handleClick = () => {
-    console.log("Hello");
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name) => {
-    console.log(`Hello ${name}`);
-  };
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button
-        onClick={() => {
-          handleClickAgain("ime");
-        }}
-      >
-        Click me again
-      </button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
