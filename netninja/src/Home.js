5702b436f98ff0540a8be439600c2826c23e811d
Marko1310 <marko.cabo13@gmail.com>
2022-11-30 17:33:02 +0100

    Basic app structure

---
+++ netninja/src/Home.js
@@ -0,0 +1,25 @@
+const Home = () => {
+  const handleClick = () => {
+    console.log("Hello");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`Hello ${name}`);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("ime");
+        }}
+      >
+        Click me again
+      </button>
+    </div>
+  );
+};
+
+export default Home;
